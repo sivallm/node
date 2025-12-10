4cfc6d8d6785fc453301c9fb30cc41f51e01ec33
sivallm <sivanagababu3@gmail.com>
2025-12-10 07:11:43 +0000

    expres js done

--- app_ui/src/mq.js
+++
@@ -1,8 +0,0 @@
-import amqp from "amqplib";
-
-export async function connectMq() {
-    const conn = await amqp.connect(process.env.RABBITMQ_URL);
-    const ch = await conn.createChannel();
-    await ch.assertQueue("test_queue");
-    return ch;
-}
